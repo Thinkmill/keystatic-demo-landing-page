9fa69eb404658e014b0ee36949ae5532ab1ef948
Simon Vrachliotis <simon@simonswiss.com>
2023-02-16 07:50:45 +1100

    Update Keystatic repo config

--- keystatic.tsx
+++ keystatic.tsx
@@ -4,8 +4,8 @@ export default config({
   storage: {
     kind: "github",
     repo: {
-      owner: process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_OWNER!,
-      name: process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG!,
+      owner: "Thinkmill",
+      name: "keystatic-starter-landing-page",
     },
   },
   collections: {
