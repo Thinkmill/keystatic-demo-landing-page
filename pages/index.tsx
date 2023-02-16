8c23715f26f8fbda945be6b52a80bdfa3c5668f7
Simon Vrachliotis <simon@simonswiss.com>
2023-02-17 09:41:55 +1100

    tweaking the tweaks

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,16 +61,16 @@ export default function Index({ testimonials, landingPage }) {
           <div className="absolute inset-0">
             <div className="sticky h-full w-full">
               {/* Blob 1 */}
-              <div className="fixed inset-x-0 grid h-1/2 -translate-x-[25%] place-items-center">
-                <div className="aspect-[10/8] w-[1000px] animate-float bg-[radial-gradient(50%_50%_at_50%_50%,#e3a9c1_0%,rgba(227,169,193,0.66)_24.48%,rgba(239,238,243,0)_100%)] opacity-80 mix-blend-color-burn [animation-delay:4s]"></div>
+              <div className="fixed inset-x-0 grid h-1/2 -translate-x-[15%] place-items-center">
+                <div className="aspect-[10/8] w-[1200px] animate-float bg-[radial-gradient(50%_50%_at_50%_50%,#e3a9c1_0%,rgba(227,169,193,0.66)_24.48%,rgba(239,238,243,0)_100%)] opacity-80 mix-blend-color-burn [animation-delay:4s]"></div>
               </div>
               {/* Blob 2 */}
               <div className="fixed inset-x-0 top-[10%] grid h-1/2  place-items-center">
-                <div className="aspect-[10/8] w-[1000px] animate-float bg-[radial-gradient(50%_50%_at_50%_50%,#cfbcdc_0%,rgba(207,188,220,0.66)_24.48%,rgba(239,238,243,0)_100%)] opacity-80 mix-blend-color-burn [animation-delay:2s]"></div>
+                <div className="animate-float2 aspect-[10/8] w-[1200px] bg-[radial-gradient(50%_50%_at_50%_50%,#cfbcdc_0%,rgba(207,188,220,0.66)_24.48%,rgba(239,238,243,0)_100%)] opacity-80 mix-blend-color-burn [animation-delay:2s]"></div>
               </div>
               {/* Blob 3 */}
-              <div className="fixed inset-x-0 grid h-1/2 translate-x-[25%] place-items-center">
-                <div className="aspect-[10/8] w-[1000px] animate-float bg-[radial-gradient(50%_50%_at_50%_50%,#f4ddcd_0%,rgba(244,221,205,0.66)_24.48%,rgba(239,238,243,0)_100%)] opacity-80 mix-blend-color-burn"></div>
+              <div className="fixed inset-x-0 grid h-1/2 translate-x-[15%] place-items-center">
+                <div className="aspect-[10/8] w-[1200px] animate-float bg-[radial-gradient(50%_50%_at_50%_50%,#f4ddcd_0%,rgba(244,221,205,0.66)_24.48%,rgba(239,238,243,0)_100%)] opacity-80 mix-blend-color-burn"></div>
               </div>
             </div>
           </div>
