b16d76f1cad47becadfe971f2e50a9332d20c7ca
Joe Prisk <joe.prisk@thinkmill.com.au>
2023-03-08 10:33:05 +1300

    added conditional storage based on node env

--- keystatic.config.tsx
+++ keystatic.config.tsx
@@ -1,13 +1,25 @@
-import { collection, config, fields, singleton } from "@keystatic/core";
+import {
+  collection,
+  config,
+  fields,
+  singleton,
+  LocalConfig,
+  GitHubConfig,
+} from "@keystatic/core";
+
+const storage: LocalConfig["storage"] | GitHubConfig["storage"] =
+  process.env.NODE_ENV === "development"
+    ? { kind: "local" }
+    : {
+        kind: "github",
+        repo: {
+          owner: process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_OWNER!,
+          name: process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG!,
+        },
+      };
 
 export default config({
-  storage: {
-    kind: "github",
-    repo: {
-      owner: process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_OWNER!,
-      name: process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG!,
-    },
-  },
+  storage,
   singletons: {
     landingPage: singleton({
       label: "Landing Page",
