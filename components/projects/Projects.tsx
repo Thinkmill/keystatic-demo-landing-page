1f672fe7541ff7e87578b83a13370753f98f2cb8
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2023-08-16 16:54:04 +1000

    adding portfolio inside a slider

--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -1,9 +1,12 @@
 import './Projects.scss';
 
 import { useState, useEffect } from "react";
+import { register } from 'swiper/element/bundle';
 
 import { IProjectData } from "./Projects.interfaces";
 import { getProjectsLocal } from "./Projects.service";
+
+register();
 
 const Projects = (props: any) => {
 	const [projects, setProjects] = useState([]);
@@ -17,59 +20,68 @@ const Projects = (props: any) => {
 
 	return (
 		<div id="projects">
+			<swiper-container
+				slides-per-view="1"
+				navigation="true"
+				pagination="true"
+				autoplay="true"
+			>
 				{projects!.length > 0 ? projects!.map((project: IProjectData) => (
-					<div className="project" key={project.id}>
-						<div className="details">
-							<div className='title'>
-								<div className="type">{project.type}</div>
+					<swiper-slide>
+						<div className="project" key={project.id}>
+							<div className="details">
+								<div className='title'>
+									<div className="type">{project.type}</div>
 
-								<div className="location" style={{ backgroundImage: `url(images/portfolio/location.png)` }}>
-									{project.location}
+									<div className="location" style={{ backgroundImage: `url(images/portfolio/location.png)` }}>
+										{project.location}
+									</div>
+								</div>
+
+								<div className="name">{project.name}</div>
+
+								<div className="images" style={{ backgroundImage: `url(${project.images![0]})` }}></div>
+
+								<div className="features">
+									<ul>
+										{project.features.map(item => {
+											return <li key={item}>{item}</li>;
+										})}
+									</ul>
+								</div>
+
+								<div className="financials">
+									<table>
+										<thead>
+											<tr>
+												<th className="text">Financials</th>
+												<th className="amount">(AUD)</th>
+											</tr>
+										</thead>
+										<tbody>
+										{
+											Object.keys(project.financials).map((key: any, i: any) => (
+												<tr key={i}>
+													<td className="text">{key.replace(/_/g,' ')}</td>
+													<td className="amount">{project.financials[key]}</td>
+												</tr>
+											))}
+										</tbody>
+									</table>
+								</div>
+
+								<div className="success">
+									<ul>
+										{project.success.map(item => {
+											return <li key={item}>{item}</li>;
+										})}
+									</ul>
 								</div>
 							</div>
-
-							<div className="name">{project.name}</div>
-
-							<div className="images" style={{ backgroundImage: `url(${project.images![0]})` }}></div>
-
-							<div className="features">
-								<ul>
-									{project.features.map(item => {
-										return <li key={item}>{item}</li>;
-									})}
-								</ul>
-							</div>
-
-							<div className="financials">
-								<table>
-									<thead>
-										<tr>
-											<th className="text">Financials</th>
-											<th className="amount">(AUD)</th>
-										</tr>
-									</thead>
-									<tbody>
-									{
-										Object.keys(project.financials).map((key: any, i: any) => (
-											<tr key={i}>
-												<td className="text">{key.replace(/_/g,' ')}</td>
-												<td className="amount">{project.financials[key]}</td>
-											</tr>
-										))}
-									</tbody>
-								</table>
-							</div>
-
-							<div className="success">
-								<ul>
-									{project.success.map(item => {
-										return <li key={item}>{item}</li>;
-									})}
-								</ul>
-							</div>
 						</div>
-					</div>
+					</swiper-slide>
 				)) : <div>{"No projects available"}</div> }
+			</swiper-container>
 		</div>
 	)
 }
