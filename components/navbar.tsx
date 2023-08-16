4d4cb65c6c96ccb53219080c7dc38f9e76511ce1
Prasada Indukuri <prasada.indukuri_migration@specialized.com>
2023-08-16 16:54:24 +1000

    adding an admin link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,34 +3,28 @@ import Link from "next/link";
 import LogoLink from "./logo-link";
 
 export default function Navbar() {
-  return (
-    <div className="relative">
-      <header className="absolute inset-x-0 z-20 py-4 sm:fixed sm:h-24 sm:bg-white/[85%] sm:py-6 sm:backdrop-blur">
-        <div className="mx-auto flex h-full max-w-5xl items-center justify-between px-6 lg:px-8">
-          <LogoLink />
-          <nav className="flex items-center justify-between sm:ml-16 sm:w-full">
-            <div className="hidden gap-10 sm:flex">
-              <a className="font-medium" href="#features">
-                Process
-              </a>
-              <a className="font-medium" href="#reviews">
-                Reviews
-              </a>
-              <a className="font-medium" href="#projects">
-                Portfolio
-              </a>
-            </div>
-            <Link
-              href="#"
-              className="rounded-full bg-black px-5 py-2.5 font-medium text-white hover:bg-gray-900"
-            >
-              Contact us
-            </Link>
-          </nav>
-        </div>
-      </header>
-    </div>
-  );
+	return (
+		<div className="relative">
+			<header className="absolute inset-x-0 z-20 py-4 sm:fixed sm:h-24 sm:bg-white/[85%] sm:py-6 sm:backdrop-blur">
+				<div className="mx-auto flex h-full max-w-5xl items-center justify-between px-6 lg:px-8">
+					<LogoLink />
+
+					<nav className="flex items-center justify-between sm:ml-16 sm:w-full">
+						<div className="hidden gap-10 sm:flex">
+							<a className="font-medium" href="#features"> Process </a>
+							<a className="font-medium" href="#reviews"> Reviews </a>
+							<a className="font-medium" href="#projects"> Portfolio </a>
+						</div>
+
+						<div className="links">
+							<Link href="/keystatic" className="rounded-full px-5 py-2.5 font-medium hidden gap-10 sm:inline"> Admin </Link>
+							<Link href="#" className="rounded-full bg-black px-5 py-2.5 font-medium text-white hover:bg-gray-900 whitespace-nowrap"> Contact us </Link>
+						</div>
+					</nav>
+				</div>
+			</header>
+		</div>
+	);
 }
 
 function KeystaticBanner() {
